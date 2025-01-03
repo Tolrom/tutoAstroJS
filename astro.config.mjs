be6afed41de12db49fc667968f2db8cfd17f8589
tolrom <max.le.morlot@gmail.com>
2025-01-03 12:29:06 +0100

    setup environment variable usage and storyblok SDK

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite';
+import tailwind from '@astrojs/tailwind';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      host: 'localhost'
+    },
+  },
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN
+    }),
+    tailwind()
+  ]
+})
